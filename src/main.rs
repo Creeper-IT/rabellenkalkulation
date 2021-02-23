7556560a19eccacc846906e81245563e7e2121f6
Moritz Kempe <moke12g@mailbox.org>
2021-02-23 19:26:11 +0100

    Rechenschleife implementiert

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::read;
+
 fn main() {
-    println!("Hello, world!");
+    let test = 54;
+    let test2:str;
+    println!("Hello, world! {0} ", test);
+
+    test2 = read!("{}\n");
+    test = test2.parse::<i32>().unwrap();
+
+    if text == 54 {
+        println!("Die Zahl ist die 54");
+    } else {
+        println!("Die Zahl ist nicht die 54");
+    }
 }
